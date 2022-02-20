56ab20256594c828d1ebb2aab562bba95debab6a
Jerome Brown <jeromebrown@JeromeBrownsMBP.lan>
2022-02-20 08:19:57 -0600

    navigation

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,35 @@
+<script>
+    import {page} from "$app/stores"
+    
+    console.log(page)
+</script>
+
+
+<nav>
+    <ul>
+        <li><a href ="/">Home</a></li>
+        <li><a href ="/about">About</a></li>
+        <li><a href ="/contact">Contact</a></li>
+        <li><a href ="/projects">Projects</a></li>
+    </ul>
+</nav>
+
+
+<slot />
+
+<style>
+    ul {
+        display: flex;
+        list-style: none;
+    }
+    li {
+        margin-right: 1em;
+        font-weight: bold;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        
+    }
+    li:hover{
+        text-decoration: underline;
+    }
+
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<h1>Jerome Brown</h1>
+<h2>Milwaukee native and multitalented innovator</h2>
+
+    
+    <style>
+	h1 {
+		color: #c63535;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        text-align: center;
+	}
+    h2 {
+		color: #000000;
+		font-size: 2em;
+		font-weight: 25;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        text-align: center;
+	}
+    </style>
